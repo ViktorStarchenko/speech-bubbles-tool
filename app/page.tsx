18089ff173db0d05f56758567397aea921f6806c
Viktor Starchenko <viktor.s@overdose.digital>
2025-07-30 14:22:00 +0300

    removed components/BubbleSquare/BubbleSquare.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import styles from "./page.module.css";
 import Main from "../components/main/main";
 import Tools from "../components/Tools/Tools";
 import Preview from "../components/Preview/Preview";
 import ImageForm from "../components/ImageForm/ImageForm";
-import BubbleSquare from "../components/BubbleSquare/BubbleSquare";
 import ContentList from "../components/contentList/ContentList";
 
 
