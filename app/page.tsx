136ca53a0771c4a782cd63f7c5f07b823146c0a6
Viktor Starchenko <viktor.s@overdose.digital>
2025-07-30 16:34:12 +0300

    added info sectin

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,20 @@ import Tools from "../components/Tools/Tools";
 import Preview from "../components/Preview/Preview";
 import ImageForm from "../components/ImageForm/ImageForm";
 import ContentList from "../components/contentList/ContentList";
+import Info from "../components/Info/Info";
 
 
 export default function Home() {
   return (
-      <Main>
-          <Preview />
-          <Tools>
-              <ImageForm />
-              <ContentList />
-          </Tools>
-      </Main>
+      <>
+          <Main>
+              <Preview />
+              <Tools>
+                  <ImageForm />
+                  <ContentList />
+              </Tools>
+          </Main>
+          <Info />
+      </>
   );
 }
---
+++ components/Info/Info.tsx
@@ -0,0 +1,20 @@
+export default function Info() {
+
+    return (
+        <div className="info-section">
+            <div className="wrapper wrapper-860">
+                <div className="content-box">
+                    <h2>Instructions</h2>
+                    <ul className="info-list">
+                        <li>Upload your image for editing</li>
+                        <li>Click Add content to add a text item.</li>
+                        <li>Choose bubble style, font style</li>
+                        <li>Drag and drop the bubble to the desired position. Adjust the size if necessary.</li>
+                        <li>When you are sure that everything is done, just click "Save image"</li>
+                        <li>We do not store your images. Your images and content settings are stored only until you refresh the page.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
+}
